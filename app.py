4cef16b2a90e1046ee2cffb0abf8cf41b8d5d369
Trevor Rempel <tjrempel87@gmail.com>
2016-07-11 01:05:50 -0400

    closer to final product

--- app.py
+++ app.py
@@ -15,8 +15,6 @@ import datetime
 app = Flask(__name__)
 GMAPS_KEY = os.getenv('GOOGLE_MAP')
 
-app.LAT = (40.477399,40.917577)
-app.LNG = (-74.25909, -73.700009)
 app.SRC = "https://maps.googleapis.com/maps/api/js?key=" +GMAPS_KEY +"&libraries=places&callback=initMap"
 app.YEAR = 2015
 days_list = ["Monday","Tuesday","Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
@@ -26,43 +24,21 @@ months_list = ["January","February","March", "April","May", "June", "July","Augu
 with open("cmap.dill", "r") as f:
 	cmap = dill.load(f)
 
-def get_Club_Data():
-	df = pd.read_csv("night.csv")
-	
-	lat_club = map(str, df["Lat"].values)
-	lng_club = map(str,df["Long"].values)
-	counts = map(str,df["percent"].values)
-	name = map(str,df["Name"].values)
-	vals  = [(counts[i],name[i],lat_club[i],lng_club[i]) for i in range(len(name))]
-	vals = sorted(vals)
-	counts = [val[0] for val in vals]
-	name  = [val[1] for val in vals]
-	lat_club = [val[2] for val in vals]
-	lng_club = [val[3] for val in vals]
-	# web = map(str,dfRest["website"].values)
-	# info = dfRest["info"]
+with open("zip_polys.dill","r") as f:
+	zip_polys = dill.load(f)
 
-	return lat_club,lng_club, counts,name
+with open("zip_club.dill","r") as f:
+	zip_club = dill.load(f)
 
 
-lat_club,lng_club,counts,names = get_Club_Data()
-#info = [json.dumps(item) for item in info]
 
-# relevant_keys = ['name','rating','formatted_address','formatted_phone_number','website']
+dfClub = pd.read_csv("night_info.csv",encoding = 'utf-8').sort_values("percent", ascending = False)
 
+club_counts,lat_club,lng_club,club_names,club_urls = zip(*dfClub[["percent","Lat","Long","Name_Yelp","url"]].values)
 
-# info = [json.dumps(item, ensure_ascii=False).encode('utf8') for item in info]
-#info = [json.dumps(make_str(item)) for item in info]
-#Load in the data that will be used
+app.tip_poly,app.tip_colors,app.tip_weight = None,None,None
+club_poly,club_colors,club_weight = None, None, None
 
-#with open('pick.pickle', 'rb') as f:
-#	dfpick = pickle.load(f) 
-
-# with open('pick.pickle', 'rb') as f:
-# 	dfpick = pickle.load(f)
-
-# with open('unfilter_pick.pickle','rb') as f:
-# 	dfunfilt = pickle.load(f)
 
 
 @app.route('/')
@@ -71,29 +47,29 @@ def main():
 
 @app.route('/index', methods = ['GET','POST'])
 def index():
+	today = datetime.datetime.today()
+	month = today.strftime("%B")
+	day = today.strftime("%A")
+	time = today.hour
 	if request.method == 'GET':
-
-
 		
 		#When the website first launches grab the current date-time and use this as an input value
-		today = datetime.datetime.today()
-		#year_today = str(today.year)
-		month_today = today.strftime("%B")
-		day_today = today.strftime("%A")
-		time_today = today.hour
-		#lat, lng = process_lat_lng(select_values(dfdrop,[app.YEAR],[month_today],[day_today],[time_today]))
-		# lat = map(str,dfunfilt["pickup_lat"].values)
-		# lng = map(str, dfunfilt["pickup_long"].values)
-		lat,lng = None, None
 
-		return render_template('index.html',src = app.SRC,plot_lat = lat,\
-			plot_lng = lng, month = month_today, day = day_today, time = time_today, time_list = range(24), month_list = months_list,\
-			day_list = days_list, lat_club = lat_club,lng_club = lng_club,counts = counts,names = names)
+		
+
+
+		return render_template('index.html',src = app.SRC, month = month, day = day, time = time, \
+			time_list = range(24), month_list = months_list, day_list = days_list, \
+			lat_club = lat_club,lng_club = lng_club,club_counts = club_counts,club_names = club_names,club_urls = club_urls)
+
+
+
 	if request.method == 'POST':
-		#dd = request.form.get("date_input")
-		#year = int(request.form.get("year"))
+		print request.form.keys()
 
-		try:
+		to_render = 'index2.html'
+		if 'month' in request.form.keys():
+
 			month = request.form.get("month")
 			day = request.form.get("day")
 			time = request.form.get("time")
@@ -114,35 +90,31 @@ def index():
 			to_load = "new_cluster_" + month_str +".dill"
 			with open(to_load, "r") as f:
 				poly_info = dill.load(f)[(month_val,day_val,time)]
-			poly = []
+			app.tip_poly = []
 			for polygon in poly_info["poly"].values:
 				lng, lat = polygon
-				poly.append([list(lng),list(lat)])
-			weight = ["{:.1f}".format(val) for val in list(poly_info["percent"].values)]
+				app.tip_poly.append([list(lng),list(lat)])
+			app.tip_weight = ["{:.1f}".format(val) for val in list(poly_info["percent"].values)]
 
-			percents = [math.log(val + 1) for val in poly_info["percent"].values]
-			MAX_VAL = max(percents)+0.001
-			percents = [val/MAX_VAL for val in percents]
+			tip_percents = [math.log(val + 1) for val in poly_info["percent"].values]
+			MAX_VAL = max(tip_percents)+0.001
+			tip_percents = [val/MAX_VAL for val in tip_percents]
 			
-			colors = [matplotlib.colors.rgb2hex(cmap(val)) for val in percents]
+			app.tip_colors = [matplotlib.colors.rgb2hex(cmap(val)) for val in tip_percents]
+			scroll = 'tip_map'
 
+		elif 'reset_club' in request.form.keys():
+			scroll = 'club_map'
 
-			lat,lng = None,None
-			# lat, lng = process_lat_lng(select_values(dfpick,[app.YEAR],[month_val],[day_val],[time]))
-		except ValueError:
-			lat, lng = None, None
+		else:
+			scroll = 'nav-bar main'
 	
 
-		#At this point we assume that the month, day and time are being passed in as a number so we select as
-		#dfGoing = select_values(dfdrop, [app.YEAR], range(month, month+1), range(day, day+1), range(time, time+1))
-		##lats, lngs = process_lat_lng(dfGoing) 
-		#print dd
-		#print "hello"
-
-		return render_template('index2.html',scroll = 'premap', src = app.SRC,plot_lat = lat, plot_lng = lng,
+		return render_template(to_render,scroll = scroll, src = app.SRC,
 			month = month, day = day, time = time, time_list = range(24), month_list = months_list,\
-			day_list = days_list,lat_club = lat_club,lng_club = lng_club,counts = counts,names= names,poly = poly,colors = colors,
-			weight = weight)
+			day_list = days_list,lat_club = lat_club,lng_club = lng_club,club_counts = club_counts,club_names= club_names,\
+			tip_poly = app.tip_poly,tip_colors = app.tip_colors,
+			tip_weight = app.tip_weight,club_urls = club_urls)
 
 
 
@@ -162,8 +134,49 @@ def index():
 		script, div=components(fig)
 		'''
 
+@app.route('/club_zipcodes', methods = ['GET','POST'])
+def club_zipcodes():
+
+	today = datetime.datetime.today()
+	month = today.strftime("%B")
+	day = today.strftime("%A")
+	time = today.hour
+
+	club = request.form.get('club')
+
+
+	grid = dfClub[dfClub["Name_Yelp"] == club]["grid"].values[0]
+	# print dfClub[dfClub["Name_Yelp"] == club][["Lat","Long"]].values
+	cur_lat,cur_lng = dfClub[dfClub["Name_Yelp"] == club][["Lat","Long"]].values[0]
+
+	l,r = grid.split(",")
+	grid_point = (int(l[1:]),int(r[:-1]))
+	zips = zip_club[grid_point]
+
+	club_poly = []
+	club_percent = []
+	club_weight = []
+	for zip_code, weight in zips.items():
+		if zip_code != 11371 and zip_code !=11430:
+			for poly in zip_polys[zip_code]:
+				lng,lat = poly
+				club_poly.append([list(lng),list(lat)])
+				club_percent.append(weight)
+				club_weight.append(zip_code)
+
+	club_weight = map(str,map(int,club_weight))
+	club_colors = [matplotlib.colors.rgb2hex(cmap(val)) for val in club_percent]
+	scroll = 'club_map'
+	to_render = 'index3.html'
+	return render_template(to_render,scroll = scroll, src = app.SRC,
+			month = month, day = day, time = time, time_list = range(24), month_list = months_list,\
+			day_list = days_list,lat_club = lat_club,lng_club = lng_club,club_counts = club_counts,club_names= club_names,\
+			tip_poly = app.tip_poly,tip_colors = app.tip_colors,
+			tip_weight = app.tip_weight,club_urls = club_urls,club_weight = club_weight,club_colors = club_colors,club_poly = club_poly,\
+			cur_info = [cur_lat, cur_lng,club])
+
 
 
 if __name__ == '__main__':
-  app.run(debug = True, port = 5001)
+  app.run(debug = True, port = 5002)
   #app.run(port=33507)
